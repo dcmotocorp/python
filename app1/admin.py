bbbc1d9d7eba5722f57b08507850433178c1955a
dcmotocorp <41550974+dcmotocorp@users.noreply.github.com>
2021-06-08 20:38:09 +0530

    Update admin.py

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Account,Branch,Loan,User,Bank,Runner
+from .models import Account, Branch, Loan, User, Bank, Runner
 admin.site.register(Bank)
 admin.site.register(Branch)
 admin.site.register(Account)
